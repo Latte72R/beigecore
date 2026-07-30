b2ed69b961e74c84d893c29e48b0c5fade47769c
Latte72R <contact@latte72.net>
2026-07-30 23:23:55 +0900

    Tang Nano 9K 用の修正

--- tang/timing.sdc
+++
@@ -1,1 +0,0 @@
-create_clock -name clk -period 37.037 -waveform {0 18.518} [get_ports {clk}]
--- tang/top_tang.sv
+++
@@ -1,4 +0,0 @@
-core_top #(
-    .MEMORY_FILEPATH_IS_ENV (0 ),
-    .MEMORY_FILEPATH        ("../test/led.hex")
-) t (
